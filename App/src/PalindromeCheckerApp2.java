a25013da8fef6678037d714571caa1386d492f6b
jayaprakash256006-coder <jayaprakash256006@gmail.com>
2026-03-10 22:19:21 +0530

    UC-13

--- App/src/PalindromeCheckerApp2.java
+++ App/src/PalindromeCheckerApp2.java
@@ -1,28 +1,30 @@
-/*
-Main class - PalindromeAPP
-Use case 1 : Printing the application as Entry Message
-
-Description:
-  This class represents the entry point of the Palindrome Checker Management System.
-
-  At this stage, the application:
-   - Starts education from the main() method
-   - Displays a welcome message
-   - Shows application version
-
-   No palindrome logic is implemented yet.
-
-   The goal is to establish a clear startup flow.
-
-   @author Kevin
-   @version 1.0
- */
-
+import java.util.Scanner;
 
 public class PalindromeCheckerApp2 {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized Successfully.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input : ");
+        String input = scanner.nextLine();
+
+        long startTime = System.nanoTime();
+
+        boolean isPalindrome = checkPalindrome(input);
+
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
+
+        System.out.println("Is Palindrome?: " + isPalindrome);
+        System.out.println("Execution Time : " + executionTime + " ns");
+
+        scanner.close();
+    }
+
+    public static boolean checkPalindrome(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equalsIgnoreCase(reversed);
     }
 }
